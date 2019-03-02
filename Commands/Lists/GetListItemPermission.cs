3762fcec9f48bdbbc347ebf615a32f3cff1bc5a4
@spjeff <spjeff@spjeff.com>
2019-03-01 18:58:37 -0600

    Get cmdlets for current Set cmdlets

---
+++ Commands/Lists/GetListItemPermission.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+using System.Management.Automation;
+using Microsoft.SharePoint.Client;
+using SharePointPnP.PowerShell.CmdletHelpAttributes;
+using SharePointPnP.PowerShell.Commands.Base.PipeBinds;
+
+namespace SharePointPnP.PowerShell.Commands.Lists
+{
+    [Cmdlet(VerbsCommon.Get, "PnPListItemPermission", DefaultParameterSetName = "User")]
+    [CmdletHelp("Get list item permissions",
+        Category = CmdletHelpCategory.Lists)]
+    [CmdletExample(
+        Code = "PS:> Get-PnPListItemPermission -List 'Documents' -Identity 1 -User 'user@contoso.com'",
+        Remarks = "This will return the permissions for the user 'user@contoso.com' and listitem with id 1 in the list 'Documents'",
+        SortOrder = 1)]
+    [CmdletExample(
+        Code = "PS:> Get-PnPListItemPermission -List 'Documents' -Identity 1",
+        Remarks = "This will return the permissions for listitem with id 1 in the list 'Documents'",
+        SortOrder = 2)]
+    public class GetListItemPermission : PnPWebCmdlet
+    {
+        [Parameter(Mandatory = true, ValueFromPipeline = true, Position = 0, HelpMessage = "The ID, Title or Url of the list.", ParameterSetName = ParameterAttribute.AllParameterSets)]
+        public ListPipeBind List;
+
+        [Parameter(Mandatory = true, ValueFromPipeline = true, HelpMessage = "The ID of the listitem, or actual ListItem object", ParameterSetName = ParameterAttribute.AllParameterSets)]
+        public ListItemPipeBind Identity;
+
+        [Parameter(Mandatory = true, ParameterSetName = "Group")]
+        public GroupPipeBind Group;
+
+        [Parameter(Mandatory = true, ParameterSetName = "User")]
+        public string User;
+
+        protected override void ExecuteCmdlet()
+        {
+            List list = null;
+            if (List != null)
+            {
+                list = List.GetList(SelectedWeb);
+            }
+            if (list != null)
+            {
+                var item = Identity.GetListItem(list);
+                if (item != null)
+                {
+                    Principal principal = null;
+                    if (ParameterSetName == "Group")
+                    {
+                        if (Group.Id != -1)
+                        {
+                            principal = SelectedWeb.SiteGroups.GetById(Group.Id);
+                        }
+                        else if (!string.IsNullOrEmpty(Group.Name))
+                        {
+                            principal = SelectedWeb.SiteGroups.GetByName(Group.Name);
+                        }
+                        else if (Group.Group != null)
+                        {
+                            principal = Group.Group;
+                        }
+                    }
+                    else
+                    {
+                        principal = SelectedWeb.EnsureUser(User);
+                        ClientContext.ExecuteQueryRetry();
+                    }
+                    if (principal != null)
+                    {
+                        var roleAssignment = item.RoleAssignments.GetByPrincipal(principal);
+                        var roleDefinitionBindings = roleAssignment.RoleDefinitionBindings;
+                        foreach (var role in roleDefinitionBindings)
+                        {
+                            WriteObject(role, true);
+                        }
+                    }
+                    else
+                    {
+                        var roleAssignment = item.RoleAssignments;
+                        var roleDefinitionBindings = roleAssignment.RoleDefinitionBindings;
+                        foreach (var role in roleDefinitionBindings)
+                        {
+                            WriteObject(role, true);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Commands/Lists/GetListPermission.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+using System.Management.Automation;
+using Microsoft.SharePoint.Client;
+using SharePointPnP.PowerShell.CmdletHelpAttributes;
+using SharePointPnP.PowerShell.Commands.Base.PipeBinds;
+
+namespace SharePointPnP.PowerShell.Commands.Lists
+{
+    //TODO: Create Test
+    [Cmdlet(VerbsCommon.Get, "PnPListPermission")]
+    [CmdletHelp("Get list permissions",
+        Category = CmdletHelpCategory.Lists)]
+    [CmdletExample(
+        Code = "PS:> Get-PnPListPermission -Identity 'Documents' -User 'user@contoso.com' -AddRole 'Contribute'",
+        Remarks = "This will return the permissions to the user 'user@contoso.com' for the list 'Documents'",
+        SortOrder = 1)]        
+    [CmdletExample(
+        Code = "PS:> Get-PnPListPermission -Identity 'Documents' -User 'user@contoso.com' -RemoveRole 'Contribute'",
+        Remarks = "This will return the permissions to the user 'user@contoso.com' for the list 'Documents'",
+        SortOrder = 2)]        
+    public class GetListPermission : PnPWebCmdlet
+    {
+        [Parameter(Mandatory = true, ParameterSetName = ParameterAttribute.AllParameterSets, HelpMessage = "The ID or Title of the list.")]
+        public ListPipeBind Identity;
+
+        [Parameter(Mandatory = false, ParameterSetName = "Group")]
+        public GroupPipeBind Group;
+
+        [Parameter(Mandatory = false, ParameterSetName = "User")]
+        public string User;
+
+        protected override void ExecuteCmdlet()
+        {
+            var list = Identity.GetList(SelectedWeb);
+
+            if (list != null)
+            {
+                Principal principal = null;
+                if (ParameterSetName == "Group")
+                {
+                    if (Group.Id != -1)
+                    {
+                        principal = SelectedWeb.SiteGroups.GetById(Group.Id);
+                    }
+                    else if (!string.IsNullOrEmpty(Group.Name))
+                    {
+                        principal = SelectedWeb.SiteGroups.GetByName(Group.Name);
+                    }
+                    else if (Group.Group != null)
+                    {
+                        principal = Group.Group;
+                    }
+                }
+                else
+                {
+                    principal = SelectedWeb.EnsureUser(User);
+                    ClientContext.ExecuteQueryRetry();
+                }
+                if (principal != null)
+                {
+                    var roleAssignment = list.RoleAssignments.GetByPrincipal(principal);
+                    var roleDefinitionBindings = roleAssignment.RoleDefinitionBindings;
+                    foreach (var role in roleDefinitionBindings)
+                    {
+                        WriteObject(role, true);
+                    }
+                }
+                else
+                {
+                    var roleAssignment = list.RoleAssignments;
+                    var roleDefinitionBindings = roleAssignment.RoleDefinitionBindings;
+                    foreach (var role in roleDefinitionBindings)
+                    {
+                        WriteObject(role, true);
+                    }
+                }
+            }
+        }
+    }
+}
